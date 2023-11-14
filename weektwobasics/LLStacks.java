ed12661a3beb37f6a334dd2a5914392b6358afcf
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2023-11-14 10:22:03 +0000

    Iterator on LLStacks

--- weektwobasics/LLStacks.java
+++ weektwobasics/LLStacks.java
@@ -3,7 +3,9 @@ package weektwobasics;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class LLStacks<Item> {
+import java.util.Iterator;
+
+public class LLStacks<Item> implements Iterable<Item>{
     private Node first;
     private class Node {
         Item item;
@@ -33,11 +35,31 @@ public class LLStacks<Item> {
         return item;
     }
 
+    public Iterator<Item> iterator() {
+        return new ListStackIterator();
+    }
+
+    public class ListStackIterator implements Iterator<Item>{
+        private Node head = first;
+        public boolean hasNext()
+        { return head != null; }
+        public Item next() {
+            Item item = head.item;
+            head = head.next;
+            return item;
+        }
+    }
+
     public static void main(String[] args) {
         LLStacks<String> stack = new LLStacks<>();
         while(!StdIn.isEmpty()) {
             String item = StdIn.readString();
             stack.push(item);
+        }
+        for( String s : stack)
+            StdOut.println(s);
+        StdOut.println("Now to pop");
+        while(!stack.isEmpty()){
             StdOut.println(stack.pop());
         }
 
