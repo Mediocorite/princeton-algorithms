3f37488f1adaa2526628ccbb25ebface7981573a
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2023-11-14 10:44:19 +0000

    LLQueues iterator done

--- weektwobasics/LLQueues.java
+++ weektwobasics/LLQueues.java
@@ -3,7 +3,9 @@ package weektwobasics;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class LLQueues<Item> {
+import java.util.Iterator;
+
+public class LLQueues<Item> implements Iterable<Item>{
     private Node first = null, last = null;
     private class Node {
         Item item;
@@ -42,11 +44,32 @@ public class LLQueues<Item> {
         return item;
     }
 
+    public Iterator<Item> iterator()
+    {return new ListQueueIterator(); }
+
+    public class ListQueueIterator implements Iterator<Item>{
+        private Node head = first;
+
+        public boolean hasNext(){
+            return head != null;
+        }
+        public Item next() {
+            Item item = head.item;
+            head = head.next;
+            return item;
+        }
+    }
+
     public static void main(String[] args) {
         LLQueues<String> queue = new LLQueues<>();
         while(!StdIn.isEmpty()) {
             String item = StdIn.readString();
             queue.enqueue(item);
+        }
+        for(String q: queue)
+            System.out.println(q);
+        System.out.println("Now for dequeue");
+        while(!queue.isEmpty()) {
             StdOut.println(queue.dequeue());
         }
     }
